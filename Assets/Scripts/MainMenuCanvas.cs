6fddda43a24f0850f9348876139dd4158cfc5b8e
unixir <piyushmayekar@gmail.com>
2019-01-23 00:49:14 +0530

    added menu enter & exit animations. fixed sound bug.

--- Assets/Scripts/MainMenuCanvas.cs
+++ Assets/Scripts/MainMenuCanvas.cs
@@ -7,20 +7,48 @@ public class MainMenuCanvas : MonoBehaviour
     public GameObject gameManager;
     public AudioSource[] audioSources;
     public Toggle soundToggle;
+    public Animator[] animators;
     void Awake()
     {
         gameManager = GameObject.FindGameObjectWithTag("GameController");
+        animators = GetComponentsInChildren<Animator>();
         gameManager.GetComponent<GameManager>().audioSources = audioSources;
         gameManager.SetActive(false);
         gameCanvas.enabled = false;
         pauseMenuCanvas.enabled = false;
         mainMenuCanvas.enabled = true;
+        //MenuEnter();
+    }
+    private void OnEnable()
+    {
+        MenuEnter();
     }
 
+    private void OnDisable()
+    {
+        MenuExit();
+    }
     public void PlayGame()
     {
+        //MenuExit();
         mainMenuCanvas.enabled = false;
         gameManager.SetActive(true);
+    }
+
+    void MenuExit()
+    {
+        foreach(Animator animator in animators)
+        {
+            animator.SetBool("Visible", false);
+        }
+    }
+
+    void MenuEnter()
+    {
+        foreach (Animator animator in animators)
+        {
+            animator.SetBool("Visible", true);
+        }
     }
 
     public void ExitGame()
